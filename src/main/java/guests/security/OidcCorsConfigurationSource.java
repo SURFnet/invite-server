481010e359ed104dd93981521e05d5b532661234
Okke Harsta <oharsta@zilverline.com>
2021-12-17 15:42:29 +0100

    Allow everything...

--- src/main/java/guests/security/OidcCorsConfigurationSource.java
+++ src/main/java/guests/security/OidcCorsConfigurationSource.java
@@ -5,15 +5,22 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class OidcCorsConfigurationSource implements CorsConfigurationSource {
 
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        List<String> allAllowed = Collections.singletonList(CorsConfiguration.ALL);
+
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.applyPermitDefaultValues();
-        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        corsConfiguration.setAllowedOriginPatterns(allAllowed);
+        corsConfiguration.setAllowedHeaders(allAllowed);
+        corsConfiguration.setAllowedMethods(allAllowed);
+        corsConfiguration.setAllowedHeaders(allAllowed);
+        corsConfiguration.setMaxAge(1800L);
         corsConfiguration.setAllowCredentials(true);
         return corsConfiguration;
     }
